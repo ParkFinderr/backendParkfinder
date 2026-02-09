8a3ff6bbbe3259d95c72253557f711c14b3b82fd
ivalhad <ivan14hadrian@gmail.com>
2026-02-09 20:12:50 +0700

    add schema ticket model

---
+++ src/models/ticketModel.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+
+// skema membuat tiket parkir
+const generateTicketSchema = Joi.object({
+  vehicleType: Joi.string().valid('mobil', 'motor').default('mobil').messages({
+    'any.only': 'Tipe kendaraan hanya boleh mobil atau motor.'
+  })
+});
+
+// cek tiket parkir
+const verifyTicketSchema = Joi.object({
+  qrCode: Joi.string().required().messages({
+    'string.empty': 'QR Code tidak terbaca.',
+    'any.required': 'QR Code wajib dikirim.'
+  })
+});
+
+module.exports = { generateTicketSchema, verifyTicketSchema };
