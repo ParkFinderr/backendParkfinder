68a2dc9fe67f5a1305a311d2f4f2e68b26a85057
ivalhad <ivan14hadrian@gmail.com>
2026-02-11 12:58:46 +0700

    add update redis config

---
+++ src/config/redis.js
@@ -0,0 +1,16 @@
+const { createClient } = require('redis');
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL
+});
+
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on('connect', () => console.log('✅ Terhubung ke Redis Server'));
+
+const connectRedis = async () => {
+  if (!redisClient.isOpen) {
+    await redisClient.connect();
+  }
+};
+
+module.exports = { redisClient, connectRedis };
